74256b147a3d5337eafa9575de4c3bbce999b673
Andreas Poehlmann <andreas@poehlmann.io>
2020-12-05 21:33:26 +0100

    ordest: fix OrderedSet behavior against modified tests

--- ordest/__init__.py
+++ ordest/__init__.py
@@ -8,7 +8,9 @@ __all__ = ["OrderedSet"]
 __author__ = "Andreas Poehlmann"
 __email__ = "andreas@poehlmann.io"
 
+from collections.abc import Iterable as _Iterable
 from collections.abc import MutableSet as _MutableSet
+from reprlib import recursive_repr as _recursive_repr
 
 try:
     from ordest._version import version as __version__
@@ -22,8 +24,6 @@ class OrderedSet(_MutableSet):
 
     Build an insertion order preserving collection of unique elements.
     """
-
-    __slots__ = ("__map",)
 
     def __init__(self, iterable=()):
         self.__map = dict.fromkeys(iterable)
@@ -52,6 +52,7 @@ class OrderedSet(_MutableSet):
     def __reversed__(self):
         return reversed(self.__map)
 
+    @_recursive_repr()
     def __repr__(self):
         return f"{type(self).__name__}({list(self)!r})"
 
@@ -79,3 +80,57 @@ class OrderedSet(_MutableSet):
     def __ior__(self, other):
         self.__map.update(dict.fromkeys(other))
         return self
+
+    # -- set methods --------------------------------------------------
+
+    def copy(self):
+        return self.__class__(self)
+
+    def union(self, *others):
+        oset = self.__class__(self)
+        for other in others:
+            oset.__ior__(other)
+        return oset
+
+    def update(self, *others):
+        for other in others:
+            self.__ior__(other)
+
+    def intersection(self, *others):
+        oset = self.__class__(self)
+        for other in others:
+            oset.__iand__(other)
+        return oset
+
+    def intersection_update(self, *others):
+        for other in others:
+            _MutableSet.__iand__(self, other)
+
+    def difference(self, *others):
+        oset = self.__class__(self)
+        for other in others:
+            oset.__isub__(other)
+        return oset
+
+    def difference_update(self, *others):
+        for other in others:
+            _MutableSet.__isub__(self, other)
+
+    symmetric_difference = _MutableSet.__xor__
+
+    def symmetric_difference_update(self, other):
+        _MutableSet.__ixor__(self, other)
+
+    def issubset(self, other):
+        if not isinstance(other, _MutableSet):
+            if not isinstance(other, _Iterable):
+                return NotImplemented
+            other = self._from_iterable(other)
+        return _MutableSet.__le__(self, other)
+
+    def issuperset(self, other):
+        if not isinstance(other, _MutableSet):
+            if not isinstance(other, _Iterable):
+                return NotImplemented
+            other = self._from_iterable(other)
+        return _MutableSet.__ge__(self, other)
